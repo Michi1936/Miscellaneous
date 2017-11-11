79e11a6a5f6d57cada370f3b7a5dcf5c8b7ad136
Michinori <andrias1936@basil.ocn.ne.jp>
2017-11-11 17:19:00 +0900

    number.hから元となった画像ファイルの名前を読み取りdatファイルの名前を作成するプログラム

---
+++ rename.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+void write(double input, FILE *fp){
+  fprintf(fp, "%f\n", input);
+}
+
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  char fName[20];
+  FILE *temp;
+  char ext[4]=".txt";
+  int i;
+  char c;
+
+  temp=fopen("num.h", "r");
+  if(temp==NULL){
+    printf("damn it!\n");
+    return -1;
+  }
+  i=0;
+  while((c=fgetc(temp))!=EOF){
+    if(c=='.'){
+      break;
+    }
+    printf("%c",c);
+    if(i>1){
+      fName[i-2]=c;
+    }
+    i++;
+   
+  }
+  i++;
+  fName[i]='\0';
+  printf("\n%s\n", fName);
+
+  strcat(fName, ext);
+  fp = fopen(fName, "w");
+
+  for(i=0; i<10; i++){
+    write((double)(i*0.1), fp);
+  }
+
+  fclose(fp);
+  fclose(temp);
+
+  return 0;
+}
